6de270fae783e667cf777a4aa0ce0f3bbd8998eb
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-05-24 23:08:13 +0800

    user update profile and can get profile data

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,16 @@ const { getAllScreams, postOneScream } = require("./handles/screams")
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-const { signup, login, uploadImage } = require('./handles/users')
 const FBAuth = require('./util/fbAuth')
+const { 
+    signup,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser }
+    = require('./handles/users')
+
+
 
 
 //get database  //screams route
@@ -18,12 +26,15 @@ app.get('/screams', getAllScreams);
 // post on scream
 app.post('/scream', FBAuth, postOneScream);
 
+
 //user routes
 //sign up route
 app.post('/signup', signup);
 //login
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 
 
